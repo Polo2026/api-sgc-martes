3bf0acaba5e6db34aff9846790d601227f30ece6
Cristian polo <polocristian1896@gmail.com>
2026-09-08 16:35:03 -0500

    Se ingresa info en Services

--- src/main/java/services/CourseServiceImpl.java
+++ src/main/java/services/CourseServiceImpl.java
@@ -1,4 +1,37 @@
 package services;
 
-public class CourseServiceImpl {
+import models.Course;
+import repositories.CourseRepository;
+
+import java.util.List;
+
+public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public CourseServiceImpl(CourseRepository courseRepository){
+        this.courseRepository = courseRepository;
+    }
+
+
+
+    @Override
+    public void save(Course course) {
+
+    }
+
+    @Override
+    public Course findById(long Id) {
+        return courseRepository.findById(Id);
+    }
+
+    @Override
+    public void deleteById(long Id) {
+        courseRepository.deleteById(Id);
+    }
+
+    @Override
+    public List<Course> findAll() {
+        return courseRepository.findAll();
+    }
 }
--- src/main/java/services/EnrollmentServiceImpl.java
+++ src/main/java/services/EnrollmentServiceImpl.java
@@ -1,4 +1,35 @@
 package services;
 
-public class EnrollmentServiceImpl {
+import models.Enrollment;
+import repositories.EnrollmentRepository;
+
+public class EnrollmentServiceImpl implements EnrollmentService {
+
+    private final EnrollmentRepository enrollmentRepository;
+
+    public EnrollmentServiceImpl(EnrollmentRepository enrollmentRepository){
+        this.enrollmentRepository = enrollmentRepository;
+    }
+
+    @Override
+    public void save(Enrollment enrollment) {
+        enrollmentRepository.save(enrollment);
+    }
+
+
+
+    @Override
+    public Enrollment findById(long Id) {
+        return enrollmentRepository.findById(Id);
+    }
+
+    @Override
+    public void deleteById(long Id) {
+        enrollmentRepository.deleteById(Id);
+    }
+
+    @Override
+    public List<Enrollment> findAll() {
+        return enrollmentRepository.findAll();
+    }
 }
